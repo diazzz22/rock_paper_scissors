8942b4a622322d18905432d31972c5544c92f919
diazzz22 <adriandiazgrueso@gmail.com>
2023-12-06 17:34:02 +0100

    funcion de jugador

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 function getComputerChoice () {
-    var numero = Math.floor(Math.random() * 1);
-    switch (0) {
-        case 0:
-            return 'piedra';
-        case 1:
-            return 'papel';
-        case 2:
-            return 'tijeras';
+    const opciones = ["piedra","papel","tijeras"];
+    const indiceAleatorio = Math.floor(Math.random() * opciones.length);
+    return opciones[indiceAleatorio];
+}
+const seleccion_pc = getComputerChoice();
+console.log(seleccion_pc);
+function getPlayerChoice() {
+    let opcion = "";
     
+    while (opcion !== 4) {
+    console.log("1. piedra");
+    console.log("2. papel");
+    console.log("3. tijera");
+    console.log("4. salir");
+
+    opcion = prompt("Elige una opción (1-4):");
+
+    switch (opcion) {
+        case "1":
+            console.log("Seleccionaste piedra");
+            let seleccionjugador = "piedra";
+            break;
+        case "2":
+            console.log("Seleccionaste papel");
+            seleccionjugador = "papel";
+            break;
+        case "3":
+            console.log("Seleccionaste tijera");
+            seleccionjugador = "tijera";
+            break;
+        case "4":
+            console.log("Seleccionaste salir");
+            break;
+        default:
+            console.log("Opción no válida. Por favor, elige una opción válida");
+            break;
+        }
     }
 }
-console.log(getComputerChoice(0,3));
+getPlayerChoice();
+/*function funcion_general (seleccion_jugador,seleccion_pc) {
+    const jugador = seleccion_jugador.toLowerCase();
+    const pc = seleccion_pc;
+}*/
